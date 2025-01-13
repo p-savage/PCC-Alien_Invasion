71cd1963e3ed8961ecdd083937e30a76275ca084
p-savage <pjslouisville502@gmail.com>
2025-01-13 16:05:18 -0500

    Added existing files to repository
    Moved the root project folder into the local repository folder so that changes made inside the project files can be committed and stored to the GitHub repository

---
+++ alien_invasion/ai_settings.py
@@ -0,0 +1,17 @@
+class Settings:
+    """A class to store all settings for Alien Invasion"""
+
+    def __init__(self):
+        """Initialize the game's settings"""
+        #Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230,230,230)
+        #Ship speed setting
+        self.ship_speed = 10.5
+        #Initialize settings for the bullets
+        self.bullet_speed = 15.5
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
+        self.bullets_allowed = 4
---
+++ alien_invasion/alien_invasion.py
@@ -0,0 +1,98 @@
+import sys
+import pygame
+
+from ai_settings import Settings
+from ship import Ship
+from bullet import Bullet
+
+class AlienInvasion:
+    """Overall class to manage game assets and behavior"""
+
+    def __init__(self):
+        """Initialize the game and create game resources"""
+        #initialize pygame
+        pygame.init()
+        #Set the clock
+        self.clock = pygame.time.Clock()
+        #call the game settings
+        self.settings = Settings()
+        #build the game display surface
+        self.screen = pygame.display.set_mode((
+            self.settings.screen_width, self.settings.screen_height
+        ))
+        pygame.display.set_caption("Alien Invasion")
+        #Call instances of object classes
+        self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
+        #Set the background color
+        self.screen.fill(self.settings.bg_color)
+
+    def run_game(self):
+        """Start the main loop for the game"""
+        while True:
+            #check for events, update accordingly, display result, 60fps
+            self._check_events()
+            self.ship.update()
+            self._update_bullets()
+            self._update_screen()
+            self.clock.tick(60)
+
+    def _check_events(self):
+        """Respond to key presses and mouse events"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+
+    def _check_keydown_events(self,event):
+        """Respond to keypresses"""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
+        elif event.key == pygame.K_f:
+            self._fire_bullet()
+
+    def _check_keyup_events(self,event):
+        """Respond to key releases"""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False    
+
+    def _fire_bullet(self):
+        """Create a new bullet and add it to the bullets group"""
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+
+    def _update_bullets(self):
+        """"Update the position of bullets and delete old bullets"""
+        #update bullet positions
+        self.bullets.update()
+        #delete old bullets
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+
+    def _update_screen(self):
+        """Update images on the screen, and flip to the new screen"""
+        #background
+        self.screen.fill(self.settings.bg_color)
+        #bullets
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet() 
+        #ship
+        self.ship.blitme()
+        #show it all
+        pygame.display.flip()
+
+if __name__ == '__main__':
+    #Make a game instance and run the game.
+    ai = AlienInvasion()
+    ai.run_game()
