28b70ffb7390ed49827138c6eae394b1a8383d47
p-savage <pjslouisville502@gmail.com>
2025-01-14 13:05:42 -0500

    <feat>(settings): add ship_limit
    Added the ship_limit counter to be used by the new GameStats class into the settings module

--- alien_invasion/ai_settings.py
+++ alien_invasion/ai_settings.py
@@ -15,9 +15,11 @@ class Settings:
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 4
-        #set the lateral speed of the alien fleet
-        self.fleet_speed = 1.0
-        #how far the fleet drops down
+        #set the lateral speed of the alien fleet (1)
+        self.fleet_speed = 1
+        #how far the fleet drops down (10)
         self.fleet_drop_speed = 10
         #fleet direction; 1 = right,  -1 = left
         self.fleet_direction = 1
+        #maxmimum number of ship collisions allowed
+        self.ship_limit = 3
